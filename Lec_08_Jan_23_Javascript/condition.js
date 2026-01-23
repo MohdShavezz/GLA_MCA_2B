e42630bd546bbcf27e359c7f036fef02aa5af592
MohdShavezz <mohdpctebtech19cse@gmail.com>
2026-01-23 10:15:43 +0530

    conditions ( if else nested , switch case, continue, break, nullish and ternary operator, logical operator )

---
+++ Lec_08_Jan_23_Javascript/condition.js
@@ -0,0 +1,99 @@
+//Condition
+//truthy : true, 'string',[],'0'
+//falsy :false, 0, -0, "",null,'undefined'
+
+// calculater
+// let ope='*'
+// let a=10, b=5
+// if(ope=='+'){
+//   console.log(a+b)
+// }else if(ope=='-'){
+//   console.log(a-b)
+// }else if(ope=='*'){
+//   console.log(a*b)
+// }else{
+//   console.log('invalid')
+// }
+
+// let ope='+'
+// let a=10, b=5
+
+// switch (ope) {
+//   case '+':
+//     console.log(a+b)
+//     break;
+//   case '-':
+//     console.log(a-b)
+//     break;
+//   case '*':
+//     console.log(a*b)
+//     break;
+//   default:
+//     console.log('invalid')
+// }
+
+
+//break , contine 
+// for (var i = 1; i < 11; i++) {
+//   // i=4
+//   if(i==5){ //even
+//     break  //4
+//   }
+//   console.log(i)
+  
+// }
+
+
+// let cond=0
+// if(cond){
+//   console.log('true')
+// }else{
+//   console.log('false')
+// }
+// TERNARY OPERATOR : short hand of if-else => ?:
+// cond?console.log('aaaaaa'):console.log('bbbbbb')
+
+//NULLISH : targets undefined and null
+// let arr=[8,7,9,0,undefined,null]
+// for(let i of arr){
+  
+//   // if(i==undefined || i==null){
+//   //   console.log('not mention')
+//   //   continue
+//   // }
+//   console.log(i??'not mention')
+// }
+
+
+// let a=true
+// let b=false
+// let c=true  
+// let d=true  
+// console.log((a&&b)||(c&&d))
+
+let cond=false
+if(cond){
+  console.log('cond is true')
+}
+
+cond&&console.log('cond is true')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
